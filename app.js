3c5ce1012984b9bc9d021d6a281a55788e21e0aa
alexander perez castro <alexanderemanuelperezcastro@gmail.com>
2024-10-13 11:44:23 -0600

    Server

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ let i = 0;
 
 let agregar = () => {
     let tarea = document.getElementById("nuevaTarea");
-    if (tarea.value == "") {alert("Tarea vacia"); return}
+    if (tarea.value == "") { alert("Tarea vacia"); return }
     let list = document.getElementById("lista");
     const eliminar = '<input type="button" value="Eliminar" onclick="eliminar(';
-    list.innerHTML += "<li class=\"tarea\" id=\"tarea" + i + "\" onclick=\"this.setAttribute('class','completado')\">" + tarea.value + eliminar + "'tarea" + i +"')\"></li>";
+    list.innerHTML += "<li class=\"tarea\" id=\"tarea" + i + "\" onclick=\"this.setAttribute('class','completado')\">" + tarea.value + eliminar + "'tarea" + i + "')\"></li>";
     lista.push(tarea.value);
     i++
 }
@@ -15,3 +15,9 @@ let eliminar = (_tarea) => {
     let tarea = document.getElementById(_tarea)
     tarea.remove()
 }
+const obtenerTareas = async () => {
+    const response = await fetch('http://localhost:3000/tareas');
+    const data = await response.json();
+    data.tareas.forEach((tarea) => agregarTareaALista(tarea));
+};
+document.addEventListener('DOMContentLoaded', obtenerTareas);
